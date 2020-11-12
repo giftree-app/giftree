cd6a961e6d84d8eb0713184ea13d35061e65afcf
Gabriela Dijkhoffz <gabriela.dijkhoffz@gmail.com>
2020-11-12 14:22:18 -0500

    add initial login and sign up functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonRow, IonCol, IonButton, IonList, IonItem, IonLabel, IonInput, IonText } from '@ionic/react';
-import './Login.scss';
-import { setIsLoggedIn, setUsername } from '../data/user/user.actions';
-import { connect } from '../data/connect';
-import { RouteComponentProps } from 'react-router';
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonPage,
+  IonButtons,
+  IonMenuButton,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonText,
+} from "@ionic/react";
+import "./Login.scss";
+import { setIsLoggedIn, setUsername } from "../data/user/user.actions";
+import { connect } from "../data/connect";
+import { RouteComponentProps } from "react-router";
 
 interface OwnProps extends RouteComponentProps {}
 
@@ -12,30 +29,52 @@ interface DispatchProps {
   setUsername: typeof setUsername;
 }
 
-interface LoginProps extends OwnProps,  DispatchProps { }
+interface LoginProps extends OwnProps, DispatchProps {}
 
-const Login: React.FC<LoginProps> = ({setIsLoggedIn, history, setUsername: setUsernameAction}) => {
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Login: React.FC<LoginProps> = ({
+  setIsLoggedIn,
+  history,
+  setUsername: setUsernameAction,
+}) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [usernameError, setUsernameError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
   const login = async (e: React.FormEvent) => {
     e.preventDefault();
+
     setFormSubmitted(true);
-    if(!username) {
+
+    if (!username) {
       setUsernameError(true);
     }
-    if(!password) {
+    if (!password) {
       setPasswordError(true);
     }
 
-    if(username && password) {
-      await setIsLoggedIn(true);
-      await setUsernameAction(username);
-      history.push('/tabs/schedule', {direction: 'none'});
+    if (username && password) {
+      let url = "https://cop4331-1.herokuapp.com/api/login";
+
+      const req = {
+        login: username,
+        password: password,
+      };
+
+      axios
+        .post(url, {
+          login: username,
+          password: password,
+        })
+        .then(async function () {
+          await setIsLoggedIn(true);
+          await setUsernameAction(username);
+          history.push("/tabs/schedule", { direction: "none" });
+        })
+        .catch(function () {
+          alert("Could not login. Please try again");
+        });
     }
   };
 
@@ -50,7 +89,6 @@ const Login: React.FC<LoginProps> = ({setIsLoggedIn, history, setUsername: setUs
         </IonToolbar>
       </IonHeader>
       <IonContent>
-
         <div className="login-logo">
           <img src="assets/img/appicon.svg" alt="Ionic logo" />
         </div>
@@ -58,43 +96,59 @@ const Login: React.FC<LoginProps> = ({setIsLoggedIn, history, setUsername: setUs
         <form noValidate onSubmit={login}>
           <IonList>
             <IonItem>
-              <IonLabel position="stacked" color="primary">Username</IonLabel>
-              <IonInput name="username" type="text" value={username} spellCheck={false} autocapitalize="off" onIonChange={e => setUsername(e.detail.value!)}
-                required>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Username
+              </IonLabel>
+              <IonInput
+                name="username"
+                type="text"
+                value={username}
+                spellCheck={false}
+                autocapitalize="off"
+                onIonChange={(e) => setUsername(e.detail.value!)}
+                required
+              ></IonInput>
             </IonItem>
 
-            {formSubmitted && usernameError && <IonText color="danger">
-              <p className="ion-padding-start">
-                Username is required
-              </p>
-            </IonText>}
+            {formSubmitted && usernameError && (
+              <IonText color="danger">
+                <p className="ion-padding-start">Username is required</p>
+              </IonText>
+            )}
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Password</IonLabel>
-              <IonInput name="password" type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Password
+              </IonLabel>
+              <IonInput
+                name="password"
+                type="password"
+                value={password}
+                onIonChange={(e) => setPassword(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
-            {formSubmitted && passwordError && <IonText color="danger">
-              <p className="ion-padding-start">
-                Password is required
-              </p>
-            </IonText>}
+            {formSubmitted && passwordError && (
+              <IonText color="danger">
+                <p className="ion-padding-start">Password is required</p>
+              </IonText>
+            )}
           </IonList>
 
           <IonRow>
             <IonCol>
-              <IonButton type="submit" expand="block">Login</IonButton>
+              <IonButton type="submit" expand="block">
+                Login
+              </IonButton>
             </IonCol>
             <IonCol>
-              <IonButton routerLink="/signup" color="light" expand="block">Signup</IonButton>
+              <IonButton routerLink="/signup" color="light" expand="block">
+                Signup
+              </IonButton>
             </IonCol>
           </IonRow>
         </form>
-
       </IonContent>
-
     </IonPage>
   );
 };
@@ -102,7 +156,7 @@ const Login: React.FC<LoginProps> = ({setIsLoggedIn, history, setUsername: setUs
 export default connect<OwnProps, {}, DispatchProps>({
   mapDispatchToProps: {
     setIsLoggedIn,
-    setUsername
+    setUsername,
   },
-  component: Login
-})
+  component: Login,
+});
