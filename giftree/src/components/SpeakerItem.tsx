c4dac880e9c6ecb3b35f33406a336c13e2af56db
aileen <akendrap@gmail.com>
2020-11-16 22:41:54 -0500

    modified groups
    - renamed groups homepage
    - removed speakers' sessions
    - styled speaker buttons

--- giftree/src/components/SpeakerItem.tsx
+++ giftree/src/components/SpeakerItem.tsx
@@ -15,35 +15,40 @@ const SpeakerItem: React.FC<SpeakerItemProps> = ({ speaker, sessions }) => {
       <IonCard className="speaker-card">
         <IonCardHeader>
           <IonItem button detail={false} lines="none" className="speaker-item" routerLink={`/tabs/speakers/${speaker.id}`}>
-            <IonAvatar slot="start">
-              <img src={process.env.PUBLIC_URL + speaker.profilePic} alt="Speaker profile pic" />
-            </IonAvatar>
+
             <IonLabel>
               <h2>{speaker.name}</h2>
               <p>{speaker.title}</p>
             </IonLabel>
           </IonItem>
         </IonCardHeader>
+        </IonCard>
+      </>
+    );
+  };
 
-        <IonCardContent>
-          <IonList lines="none">
-            {sessions.map(session => (
-              <IonItem detail={false} routerLink={`/tabs/speakers/sessions/${session.id}`} key={session.name}>
-                <IonLabel>
-                  <h3>{session.name}</h3>
-                </IonLabel>
-              </IonItem>
-            ))}
-            <IonItem detail={false} routerLink={`/tabs/speakers/${speaker.id}`}>
-              <IonLabel>
-                <h3>About {speaker.name}</h3>
-              </IonLabel>
-            </IonItem>
-          </IonList>
-        </IonCardContent>
-      </IonCard>
-    </>
-  );
-};
+        // Icons with each speaker
+        // <IonAvatar slot="start">
+        //   <img src={process.env.PUBLIC_URL + speaker.profilePic} alt="Speaker profile pic" />
+        // </IonAvatar>
+
+        // Series of "sessions" underneath each speaker
+        // <IonCardContent>
+          // <IonList lines="none">
+          //   {sessions.map(session => (
+          //     <IonItem detail={false} routerLink={`/tabs/speakers/sessions/${session.id}`} key={session.name}>
+          //       <IonLabel>
+          //         <h3>{session.name}</h3>
+          //       </IonLabel>
+          //     </IonItem>
+          //   ))}
+          //   <IonItem detail={false} routerLink={`/tabs/speakers/${speaker.id}`}>
+          //     <IonLabel>
+          //       <h3>About {speaker.name}</h3>
+          //     </IonLabel>
+          //   </IonItem>
+          // </IonList>
+        // </IonCardContent>
+
 
 export default SpeakerItem;
